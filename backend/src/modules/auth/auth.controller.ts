2df72510af58fc3b0984799e4e44d7876b8f2777
denionline <danielximenes164@gmail.com>
2026-03-01 15:32:21 +0000

    feat: Start implement the cookies

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import type { User } from 'generated/prisma/client';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,7 +10,15 @@ export class AuthController {
 
 	@UseGuards(LocalAuthGuard)
 	@Post('login')
-	login(@Req() req: { user: User }) {
-		return this.service.login(req.user);
+	login(@Req() req: { user: User }, @Res() res: Response) {
+		const tokens = this.service.login(req.user);
+
+		res.cookie('refresh_token', tokens?.refresh_token, {
+			httpOnly: true,
+			secure: process.env.DEVELOPMENT === 'production',
+			sameSite: 'strict',
+			maxAge: 1 * 24 * 60 * 60 * 1000,
+		});
+		return { access_token: tokens?.access_token };
 	}
 }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -19,13 +19,31 @@ export class AuthService {
 		return null;
 	}
 
-	login(user: User | undefined) {
+	login(user: User | undefined): {
+		access_token: string;
+		refresh_token: string;
+	} | null {
 		if (!user) {
 			return null;
 		}
-		const payload = { sub: user.id };
-		return {
-			access_token: this.jwtService.sign(payload),
-		};
+		const payload = { sub: user.id, username: user.username };
+
+		const access_token = this.jwtService.sign(payload, { expiresIn: '60s' });
+		const refresh_token = this.jwtService.sign(payload, { expiresIn: '1d' });
+
+		return { access_token, refresh_token };
+	}
+
+	refresh(refresh_token: string) {
+		const payload: {
+			sub: string;
+			username: string;
+		} = this.jwtService.verify(refresh_token);
+
+		const newAccessToken = this.jwtService.sign(
+			{ sub: payload.sub, username: payload.username },
+			{ expiresIn: '60s' },
+		);
+		return { access_token: newAccessToken };
 	}
 }
