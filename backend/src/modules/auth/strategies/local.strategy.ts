9df885caacd2a60dc4a8aa8902fe1d100b59bcd4
denionline <danielximenes164@gmail.com>
2026-03-01 15:32:38 +0000

    fix: Remove the email to not share sensitive info

--- backend/src/modules/auth/strategies/local.strategy.ts
+++ backend/src/modules/auth/strategies/local.strategy.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
 	constructor(private authService: AuthService) {
-		super({ usernameField: 'email' });
+		super();
 	}
 	async validate(email: string, password: string) {
 		const user = await this.authService.validateUser(email, password);
